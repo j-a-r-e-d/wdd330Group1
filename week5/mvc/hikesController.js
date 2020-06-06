1cdb29aee96d2285a14e4fbbf3fd483aa1d3eefa
Daniel Moster <dmoster@Daniels-MacBook-Pro.local>
2020-06-06 11:33:04 -0400

    w5 touchevent to click

--- week5/mvc/hikesController.js
+++ week5/mvc/hikesController.js
@@ -30,10 +30,11 @@ export default class HikesController {
   }
   // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
   addHikeListener() {
+    console.log(`In addHikeListener()!`)
     // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
     const childrenArray = Array.from(this.parentElement.children);
     childrenArray.forEach(child => {
-      child.addEventListener('touchend', e => {
+      child.addEventListener('click', e => {
         // why currentTarget instead of target?
         this.showOneHike(e.currentTarget.dataset.name);
       });
